e11e5e2f3df6a599cb51538c567da691d12bb63e
Tanakaryuki <tnk3257.rt@gmail.com>
2024-03-07 10:33:16 +0000

    ルーターを定義

--- api/routers/book.py
+++ api/routers/book.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, APIRouter,Depends
+from fastapi import FastAPI, APIRouter,Depends,HTTPException,status
 
 import api.schemas.book as book_schema
+import api.cruds.book as book_crud
 from api.db import get_db
 
 router = APIRouter()
@@ -11,20 +12,36 @@ async def root():
 
 @router.get("/books",response_model=book_schema.BookListResponse,description="Get all books",tags=["book"])
 def get_books(db = Depends(get_db)):
-    pass
+    books = book_crud.get_books(db)
+    return book_schema.BookListResponse(books=books)
 
 @router.post("/book",description="Create a new book",tags=["book"])
 def create_book(request: book_schema.BookCreateRequest):
-    pass
+    book = book_crud.create_book(request)
+    if not book:
+        raise HTTPException(status.HTTP_400_BAD_REQUEST)
+    
+    return HTTPException(status.HTTP_201_CREATED)
 
 @router.get("/book/{book_id}",response_model=book_schema.BookDetailResponse,description="Get a book by ID",tags=["book"])
 def get_book(book_id: str, db = Depends(get_db)):
-    pass
+    book = book_crud.get_book(book_id, db)
+    if not book:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return book_schema.BookDetailResponse(book)
 
 @router.delete("/book/{book_id}",description="Delete a book by ID",tags=["book"])
 def delete_book(book_id: str, db = Depends(get_db)):
-    pass
+    book = book_crud.get_book(book_id, db)
+    if not book:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    book_crud.delete_book(book_id, db)
+    return HTTPException(status.HTTP_200_OK)
 
 @router.put("/book/{book_id}",description="Update a book by ID",tags=["book"])
 def update_book(book_id: str, request: book_schema.BookCreateRequest, db = Depends(get_db)):
-    pass
+    book = book_crud.get_book(book_id, db)
+    if not book:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    book_crud.update_book(book_id, request, db)
+    return HTTPException(status.HTTP_200_OK)
