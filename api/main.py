926f3a149117119e0473d80ce7144f8493b07039
Tanakaryuki <tnk3257.rt@gmail.com>
2024-03-08 03:12:02 +0000

    CROSの修正

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from dotenv import load_dotenv
 
@@ -9,3 +10,14 @@ from api.routers import book, word
 app = FastAPI()
 app.include_router(book.router, prefix="/api")
 app.include_router(word.router, prefix="/api")
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
